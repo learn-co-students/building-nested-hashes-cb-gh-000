c2d93bfac121d67959f81d85c9ab423036248f7e
Brennan Brown <brennan.brown@edu.sait.ca>
2017-08-12 20:52:10 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,3 +1,7 @@
+# Building Nested Hashes: Manipulating the Hash
+
+# Method demonstrates accessing the values in a nested hash and modifying
+# the value of a particular key before returning the newly modified hash.
 def bonus
   epic_tragedy = {
    :montague => {
@@ -8,22 +12,21 @@ def bonus
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
 
-  #code your solution here:
-
-  
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  # In order to access and manipulate a nested hash key, chain all previous
+  # keys until the method can reach the key that points to the value to access.
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   epic_tragedy
 end
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,23 @@
+# Building Nested Hashes: Character Roles
+
+# Method is filling out the empty 'epic_tragedy' variable values of the
+# :montague and :capulet key. There are four keys in both hashes.
+# This allows the variable to be ready to fill out the empty hashes that
+# constitute the values for the :patriarch, :matriarch and :hero/:heroine keys,
+# nested inside the family name keys.
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
-
-  
+     :montague => {
+        :patriarch => {},
+        :matriarch => {},
+        :hero => {},
+        :hero_friends => []
+     },
+     :capulet => {
+        :patriarch => {},
+        :matriarch => {},
+        :heroine => {},
+        :heroine_friends => []
+     }
+  }
 end
